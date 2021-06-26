61b94c1ddf1ef602dc01d87c2a99651d0405d93b
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2021-06-25 22:22:13 -0400

    additional conditional compiler directives, fix compile error with non MSU setups

--- Marlin/src/module/planner.cpp
+++ Marlin/src/module/planner.cpp
@@ -1887,13 +1887,18 @@ bool Planner::_populate_block(block_t * const block, bool split_move,
   #if EITHER(PREVENT_COLD_EXTRUSION, PREVENT_LENGTHY_EXTRUDE)
     if (de) {
       #if ENABLED(PREVENT_COLD_EXTRUSION)
-        if (!msu.idler_is_homing){
-        if (thermalManager.tooColdToExtrude(extruder)) {
-          position.e = target.e; // Behave as if the move really took place, but ignore E part
-          TERN_(HAS_POSITION_FLOAT, position_float.e = target_float.e);
-          de = 0; // no difference
-          SERIAL_ECHO_MSG(STR_ERR_COLD_EXTRUDE_STOP);
-        }}
+        #if ENABLED(MSU)
+          if (!msu.idler_is_homing()){
+        #endif
+            if (thermalManager.tooColdToExtrude(extruder)) {
+              position.e = target.e; // Behave as if the move really took place, but ignore E part
+              TERN_(HAS_POSITION_FLOAT, position_float.e = target_float.e);
+              de = 0; // no difference
+              SERIAL_ECHO_MSG(STR_ERR_COLD_EXTRUDE_STOP);
+            }
+        #if ENABLED(MSU)
+          }
+        #endif
       #endif // PREVENT_COLD_EXTRUSION
       #if ENABLED(PREVENT_LENGTHY_EXTRUDE)
         const float e_steps = ABS(de * e_factor[extruder]);
