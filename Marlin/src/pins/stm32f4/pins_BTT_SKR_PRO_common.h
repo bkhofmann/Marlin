dfea83c079f38a92b9329d083dd5989beb30e996
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2021-10-28 08:51:01 -0400

    turn to laser engraver

--- Marlin/src/pins/stm32f4/pins_BTT_SKR_PRO_common.h
+++ Marlin/src/pins/stm32f4/pins_BTT_SKR_PRO_common.h
@@ -242,7 +242,7 @@
 #define HEATER_1_PIN                        PD14  // Heater1
 #define HEATER_2_PIN                        PB0   // Heater1
 #define HEATER_BED_PIN                      PD12  // Hotbed
-#define FAN_PIN                             PE5   // Fan0
+#define FAN_PIN                             PB0   // Fan0
 #define FAN1_PIN                            PC8   // Fan1
 #define FAN2_PIN                            PE6   // Fan2
 
