a15fceb643c6f6d8e605efd7b4a3634f96bc1bc8
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2022-01-26 17:13:48 -0500

    Update msu.h

--- Marlin/src/feature/msu/msu.h
+++ Marlin/src/feature/msu/msu.h
@@ -10,7 +10,7 @@ public:
     //add loop for error manadgment later
     static void idler_servo_init();
     static void tool_change(uint8_t index);
-    static void idler_home();
+    static void idler_park();
     static bool idler_is_homing();
     static void edit_MSU_BOWDEN_TUBE_SETUP_length(const float diff);
     static void move_extruder(float diff, uint8_t extruderNumber,const_feedRate_t speed, bool moveBothExtruders = false);
