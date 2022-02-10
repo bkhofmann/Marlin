968ebd79cd6e0584d60777f48246306d5194cb26
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2022-02-09 23:11:56 -0500

    refactor, bowden tube setup done

--- Marlin/src/feature/msu/msu-refactor.cpp
+++ Marlin/src/feature/msu/msu-refactor.cpp
@@ -4,10 +4,9 @@
 
 #include "msu.h"
 #include "../../module/servo.h"
+#include "../../module/planner.h"
 
-float selected_filament_nbr=-1;
-float absolute_position=0;
-
+float selected_filament_nbr = -1;
 float idler_first_filament_pos = 30;
 float idler_angle_between_bearing = 26;
 float bowdenTubeLength = MSU_BOWDEN_TUBE_LENGTH;
@@ -18,20 +17,48 @@ xyze_pos_t position;
 
 float steps_per_mm_correction_factor = 1;
 #if ENABLED(MSU_DIRECT_DRIVE_LINKED_EXTRUDER_SETUP)
-  steps_per_mm_correction_factor =  static_cast<float>(planner.settings.axis_steps_per_mm[E_AXIS]);
+steps_per_mm_correction_factor = static_cast<float>(planner.settings.axis_steps_per_mm[E_AXIS]);
 #endif
-void MSUMP::tool_change(uint8_t index){
-  if(selected_filament_nbr>=0){
-    #if ENABLED(MSU_DIRECT_DRIVE_SETUP)
-      idler_select_filament_nbr(selected_filament_nbr);
-    #endif //MSU_DIRECT_DRIVE_SETUP
-  }
+void MSUMP::tool_change(uint8_t index)
+{
+  idler_select_filament_nbr(selected_filament_nbr);
+  move_extruder(-MSU_BOWDEN_TUBE_LENGTH,25);
+  idler_select_filament_nbr(index);
+  selected_filament_nbr=index;
+  move_extruder(MSU_BOWDEN_TUBE_LENGTH,25);
+
 }
 
-//move idler to specific filament selection, -1 to park the idler
-void MSUMP::idler_select_filament_nbr(int index){
-  if(index==-1)MOVE_SERVO(MSU_SERVO_IDLER_NBR,270);
-  else MOVE_SERVO(MSU_SERVO_IDLER_NBR,MSU_SERVO_OFFSET+(index+1)*MSU_BEARING_ANGLES);
+void MSUMP::move_extruder(float dist, const_feedRate_t speed, bool moveBothExtruders)
+{
+
+  const float old = current_position.e;
+  current_position.e += dist;
+  planner.buffer_line(current_position, speed, MSU_EXTRUDER_NBR);
+  current_position.e = old;
+  planner.set_e_position_mm(old);
+
+  #if ENABLED(MSU_DIRECT_DRIVE_SETUP)
+    if (moveBothExtruders)
+    {
+      const float old = current_position.e;
+      current_position.e += dist;
+      planner.buffer_line(current_position, speed, MSU_ORIGINAL_EXTRUDER_NBR);
+      current_position.e = old;
+      planner.set_e_position_mm(old);
+    }
+  #endif
+
+  planner.synchronize();
+}
+
+// move idler to specific filament selection, -1 to park the idler
+void MSUMP::idler_select_filament_nbr(int index)
+{
+  if (index == -1)
+    MOVE_SERVO(MSU_SERVO_IDLER_NBR, 270);
+  else
+    MOVE_SERVO(MSU_SERVO_IDLER_NBR, MSU_SERVO_OFFSET + (index + 1) * MSU_BEARING_ANGLES);
 }
 
 #endif
