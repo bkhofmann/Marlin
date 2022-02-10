3de4cd5402d95f76975aabd0ef4c5fd63aa5ce13
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2022-02-09 23:50:40 -0500

    remove deprecated code related to stepper idler support

--- Marlin/src/gcode/calibrate/G28.cpp
+++ Marlin/src/gcode/calibrate/G28.cpp
@@ -62,10 +62,6 @@
   #include "../../feature/spindle_laser.h"
 #endif
 
-#if ENABLED(MSU)
-  #include "../../feature/msu/msu.h"
-#endif
-
 #define DEBUG_OUT ENABLED(DEBUG_LEVELING_FEATURE)
 #include "../../core/debug_out.h"
 
@@ -469,10 +465,6 @@ void GcodeSuite::G28() {
     }
 
   #endif // DUAL_X_CARRIAGE
-
-  #if ENABLED(MSU)
-    msu.idler_home();
-  #endif
 
   endstops.not_homing();
 
