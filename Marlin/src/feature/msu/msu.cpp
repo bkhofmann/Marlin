5ea43130effdbdbe42f2fd486238aec0063f9d93
Pierre Masselot <72809488+PierreMasselot1@users.noreply.github.com>
2022-03-30 19:43:49 -0400

    Add speed setting for load and unload

--- Marlin/src/feature/msu/msu.cpp
+++ Marlin/src/feature/msu/msu.cpp
@@ -22,29 +22,29 @@ steps_per_mm_correction_factor = MSU_EXTRUDER_STEPS_PER_MM/static_cast<float>(pl
 void MSUMP::tool_change(uint8_t index)
 {
   #if ENABLED(MSU_DIRECT_DRIVE_SETUP)
-    move_extruder(-MSU_GEAR_LENGTH,25);
+    move_extruder(-MSU_GEAR_LENGTH,MSU_SPEED);
   #endif
 
   #if ENABLED(MSU_DIRECT_DRIVE_LINKED_EXTRUDER_SETUP)
-    move_extruder(-MSU_GEAR_LENGTH,25);
+    move_extruder(-MSU_GEAR_LENGTH,MSU_SPEED);
   #endif
 
   idler_select_filament_nbr(selected_filament_nbr);
-  move_extruder(-MSU_BOWDEN_TUBE_LENGTH*steps_per_mm_correction_factor,25);
+  move_extruder(-MSU_BOWDEN_TUBE_LENGTH*steps_per_mm_correction_factor,MSU_SPEED);
   idler_select_filament_nbr(index);
   selected_filament_nbr=index;
-  move_extruder(MSU_BOWDEN_TUBE_LENGTH*steps_per_mm_correction_factor,25);
+  move_extruder(MSU_BOWDEN_TUBE_LENGTH*steps_per_mm_correction_factor,MSU_SPEED);
 
   #if ENABLED(MSU_DIRECT_DRIVE_SETUP)
-    move_extruder(4,25);
+    move_extruder(4,MSU_SPEED);
     idler_select_filament_nbr(-1);
-    move_extruder(MSU_GEAR_LENGTH,25);
+    move_extruder(MSU_GEAR_LENGTH,MSU_SPEED);
   #endif
 
   #if ENABLED(MSU_DIRECT_DRIVE_LINKED_EXTRUDER_SETUP)
-    move_extruder(4,25);
+    move_extruder(4,MSU_SPEED);
     idler_select_filament_nbr(-1);
-    move_extruder(MSU_GEAR_LENGTH,25);
+    move_extruder(MSU_GEAR_LENGTH,MSU_SPEED);
   #endif
 }
 
